4049411f3ac0c8ac4c65d6f79dcd027149dc7328
Isaac Dunnett <is560393@dal.ca>
2022-11-25 02:09:26 -0400

    fixed dashboard link on profile page

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -5,7 +5,7 @@ import ProfileForm from '../../components/user/ProfileForm'
 const Profile = () => {
   return (
     <div className="relative flex flex-col items-center justify-center gap-6 pt-16">
-      <Link href="/app" className="flex items-center text-sm text-slate-500">
+      <Link href="/" className="flex items-center text-sm text-slate-500">
         <IoIosArrowBack />
         Dashboard
       </Link>
