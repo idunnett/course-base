27fff37d66659c2f55913c9f7894e5d24894265b
Isaac Dunnett <is560393@dal.ca>
2022-11-25 01:39:17 -0400

    removed zod

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,26 +3,26 @@ import CourseWidget from '../../components/course/CourseWidget'
 import { trpc } from '../../utils/trpc'
 
 const Dashboard = () => {
-  const { data: courses = null } = trpc.course.getMyCourses.useQuery(
-    undefined,
-    {
-      refetchOnWindowFocus: false,
-      retry: false,
-    }
-  )
-  const { data: tasks = null } = trpc.task.getMyTasks.useQuery(undefined, {
-    refetchOnWindowFocus: false,
-    retry: false,
-  })
+  // const { data: courses = null } = trpc.course.getMyCourses.useQuery(
+  //   undefined,
+  //   {
+  //     refetchOnWindowFocus: false,
+  //     retry: false,
+  //   }
+  // )
+  // const { data: tasks = null } = trpc.task.getMyTasks.useQuery(undefined, {
+  //   refetchOnWindowFocus: false,
+  //   retry: false,
+  // })
 
-  if (courses && tasks)
-    return (
-      <div className="relative grid h-full w-full grid-cols-1 gap-4 overflow-auto p-4 pt-16 lg:grid-cols-2">
-        {courses.map((course) => (
-          <CourseWidget key={course.id} course={course} tasks={tasks} />
-        ))}
-      </div>
-    )
+  // if (courses && tasks)
+  //   return (
+  //     <div className="relative grid h-full w-full grid-cols-1 gap-4 overflow-auto p-4 pt-16 lg:grid-cols-2">
+  //       {courses.map((course) => (
+  //         <CourseWidget key={course.id} course={course} tasks={tasks} />
+  //       ))}
+  //     </div>
+  //   )
 
   return (
     <div className="flex h-full w-full items-center justify-center">
