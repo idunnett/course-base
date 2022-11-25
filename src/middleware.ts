d213c94ae15a567b79e6f0f07c2adb61dee02c93
Isaac Dunnett <is560393@dal.ca>
2022-11-25 02:04:29 -0400

    redirecting in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
 const publicPaths = ['/', '/auth/signin', '/auth/signup']
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   if (
     pathname.startsWith('/_next/') ||
@@ -14,7 +15,11 @@ export function middleware(request: NextRequest) {
   )
     return NextResponse.next()
 
-  if (request.cookies.has('next-auth.session-token')) return NextResponse.next()
+  const session = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  })
+  if (session) return NextResponse.next()
 
   if (!publicPaths.includes(request.nextUrl.pathname)) {
     console.log(`${request.nextUrl.pathname} redirect`)
